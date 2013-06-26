7699ee1aafdcaa0255a3e1787a4d8b4988acc77a
hepx <hepanxi@163.com>
2013-06-26 17:04:36 +0800

    增加登录检查拦截器
    Signed-off-by: hepx <hepanxi@163.com>

---
+++ src/main/java/com/xixi/web4j/interceptor/loginInterceptor.java
@@ -0,0 +1,41 @@
+package com.xixi.web4j.interceptor;
+
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.map.ser.FilterProvider;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.xixi.web4j.model.UserInfoBean;
+/**
+ * 检查登录拦截器
+ * @author xixi
+ * @date 2013-6-26
+ *
+ */
+public class loginInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		// TODO Auto-generated method stub
+		String path=request.getServletPath();
+		//不好配置这个放行的URL，所以硬编码了
+		if("/".equals(path)||"/login".equals(path)||path.startsWith("/resources")){
+			return true;
+		}else{
+			UserInfoBean loginUser=(UserInfoBean)request.getSession().getAttribute("loginUser");
+			if(loginUser!=null&&loginUser.getUserName()!=null){//用户已登录
+				return true;
+			}else{//没有登录
+				response.sendRedirect(request.getContextPath()+"/");
+				return false;
+			}
+		}
+		
+	}
+
+}
--- src/main/webapp/WEB-INF/views/main/home.jsp
+++ src/main/webapp/WEB-INF/views/main/home.jsp
@@ -10,8 +10,16 @@
 	<div class="container">
 		<div class="hero-unit">
 			<h2>欢迎使用WEB4J</h2>
-			<P>系统后端采用Spring MVC、SPRING JDBC+ECH缓存</P>
-			<P>前端采用Jquery、Bootstrap</P>
+			<P>系统采用SpringMVC、SPRING-Jdbc、Jquery、Bootstrap开发</P>
+			<br>
+			<p>系统特点：</p>
+			<ul>
+				<li>实现了简单的用户、模块、权限管理</li>
+				<li>采用RESTful</li>
+				<li>增加cache</li>
+				<li>采用SPRING验证</li>
+				<li>全局JSON转换</li>
+			</ul>
 		</div>
 	</div>
 </body>
